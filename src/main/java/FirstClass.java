0e78b839616a5af8f8b96cbf29d11f069e5e1102
Sofiia Bondarenko <soniabondarenko1@gmail.com>
2022-11-09 21:03:53 +0100

    added git commands description

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,4 +1,8 @@
 public class FirstClass {
+    // to create git repo -> git init
+    // to track files by git -> git add "filename" or git add -u or git add .
+    // to make commit -> git commit -m "your message"
+    // to see commit history -> git log
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
