2b56070fe5dd01ea7ed736248f49991d8487e617
Sofiia Bondarenko <soniabondarenko1@gmail.com>
2022-11-09 21:23:02 +0100

    added git branch description

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -3,6 +3,9 @@ public class FirstClass {
     // to track files by git -> git add "filename" or git add -u or git add .
     // to make commit -> git commit -m "your message"
     // to see commit history -> git log
+    // to go to other commit -> git checkout "commit hash"
+    // to create new branch -> get checkout -b "branch name"
+    // to switch to exiting branch -> get checkout "branch name"
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
