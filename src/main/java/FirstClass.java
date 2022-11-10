a5685102a6e89aaa5b26a9eaa9c68832e7cf5797
Andriy Gaponov <wmconstructor@gmail.com>
2022-11-10 15:43:28 +0200

    error correction

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -4,8 +4,8 @@ public class FirstClass {
     // to make commit -> git commit -m "your message"
     // to see commit history -> git log
     // to go to other commit -> git checkout "commit hash"
-    // to create new branch -> get checkout -b "branch name"
-    // to switch to exiting branch -> get checkout "branch name"
+    // to create new branch -> git checkout -b "branch name"
+    // to switch to exiting branch -> git checkout "branch name"
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
