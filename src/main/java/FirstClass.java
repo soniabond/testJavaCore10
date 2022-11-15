55af4192c79382007893d4456a666a70f61e7bf6
Sofiia Bondarenko <soniabondarenko1@gmail.com>
2022-11-15 11:35:25 +0200

    primitives

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -7,9 +7,55 @@ public class FirstClass {
     // to create new branch -> git checkout -b "branch name"
     // to switch to exiting branch -> git checkout "branch name"
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //        byte a = 15;
+//        //System.out.println((byte)(-128 + (-1)));
+//
+//
+//        float b = 9.9f;
+//        boolean c = false;
+//        BigDecimal bigDecimal = new BigDecimal("2.6548426526548943165316498464");
+//        //System.out.println(1f / 3f);
+//
+//        char charA = 'A';
+//        System.out.println("charA = " + (int)charA);
 
-        SecondClass secondClass = new SecondClass();
-        secondClass.sayHello("Andriy");
+//        int initVal = 5;
+//        long longVal = initVal;
+//
+//        long a = 50;
+//        int b = (int) a;
+//        long c = 150;
+//
+//        sum((int)a, (int)c);
+
+        Integer a = null;
+        System.out.println("a = " + a);
+        int val = 1;
+        Integer intObject = val; // inboxing
+        int val1 = intObject; // outboxing
+
+        //instanceOf
+
+        // + - * / %
+        //double i = 2;
+        //int res = 7f / 2;
+        //float res = 7 / 2;
+
+        int res = 7 % 2;
+        System.out.println("res = " + res);
+
+        res = res + 5;
+        res = res * 6;
+
+        res += 5;
+        res *= 6;
+        res /= 7;
+
+        int i = 5;
+        i += 1;
+        i -= 1;
+        // unary operations
+        i++;
+        i--;
     }
 }
--- src/main/java/SecondClass.java
+++
@@ -1,9 +0,0 @@
-/**
- * Test class
- */
-public class SecondClass {
-
-    public void sayHello(String name){
-        System.out.println("Hello, " + name);
-    }
-}
